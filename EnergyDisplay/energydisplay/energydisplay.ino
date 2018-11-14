391fe7543512f3e8f04f4dc329e038e59867fd00
Lasse Moench <lasse.moench@rwth-aachen.de>
2018-11-14 21:48:59 +0100

    Small changes to energydisplay

--- EnergyDisplay/energydisplay/energydisplay.ino
+++ EnergyDisplay/energydisplay/energydisplay.ino
@@ -10,18 +10,12 @@ LiquidCrystal_I2C lcd(0x3F, 16, 2);
 const char* ssid = "Bergernetz"; // your wireless network name (SSID)
 const char* password = "GrummelKeks1"; // your Wi-Fi network password
 const char* apiIP = "192.168.0.38";
+WiFiClient client;
+String result;
+int lcdpos = 0;
 
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi");
-  pinMode(IRLED, OUTPUT);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("Connected to WiFi!");
 
   // The begin call takes the width and height. This
   // Should match the number provided to the constructor.
@@ -33,18 +27,23 @@ void setup() {
 
   // Move the cursor characters to the right and
   // zero characters down (line 1).
-  lcd.setCursor(5, 0);
+  lcd.setCursor(0, 0);
 
   // Print HELLO to the screen, starting at 5,0.
-  lcd.print("HELLO");
+  lcd.print("Connecting...");
+  
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
 
-  // Move the cursor to the next line and print
-  // WORLD.
-  lcd.setCursor(5, 1);      
-  lcd.print("WORLD");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to WiFi!");
 }
 
 void loop() {
+  lcd.backlight();
   if (client.connect(apiIP, 80)){
     // we are connected to the host!
     client.print(String("GET /lcdinfo") + " HTTP/1.1\r\n" +
@@ -55,14 +54,31 @@ void loop() {
     Serial.println("Request info from API!");
     while (client.connected()){
       if (client.available()){
-        String line = client.readStringUntil('\n');
-        Serial.println(line);
+        result = client.readStringUntil('\n');
+        Serial.println(result);
       }
     }
     client.stop();
     Serial.println("\n[Disconnected]");
+    float wh = result.toFloat();
+    int lcdpos = 13 - result.length();
+    lcd.clear();
+    lcd.setCursor(lcdpos,0);
+    lcd.print(result);
+    lcd.setCursor(14,0);
+    lcd.print("Wh");
+    float cost = 0.0002775 * wh;
+    String coststr = String(cost);    
+    lcdpos = 12 - coststr.length();
+    lcd.setCursor(lcdpos, 1);
+    lcd.print(coststr);
+    lcd.setCursor(13, 1);
+    lcd.print("EUR");
   }else{
     // connection failure
     Serial.println("Connection to tick-API failed!");        
-  }  
+  }
+  delay(5000);
+  lcd.backlight();
+  delay(30000);
 }
